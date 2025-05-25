9b420947e8ea19954cb5b5724bfc032433970287
Jakub Stępkowski <stepkos@wp.pl>
2025-05-26 00:52:44 +0200

    feat: make connector singleton object

--- connector.py
+++ connector.py
@@ -1,22 +1,63 @@
 import pyodbc
+from threading import Lock
 
-server = 'localhost'
-database = 'AdventureWorks2019'
 
-conn_str = (
-    'DRIVER={ODBC Driver 17 for SQL Server};'
-    f'SERVER={server};'
-    f'DATABASE={database};'
-    f'Trusted_Connection=yes;'
-)
+class MSSQLConnector:
+    _instance = None
+    _lock = Lock()
 
-conn = pyodbc.connect(conn_str)
-cursor = conn.cursor()
+    def __new__(cls, *args, **kwargs):
+        if not cls._instance:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = super(MSSQLConnector, cls).__new__(cls)
+        return cls._instance
 
-cursor.execute("SELECT 1")
-rows = cursor.fetchall()
+    def __init__(self, server, database, username=None, password=None, trusted_connection=True):
+        if hasattr(self, 'initialized'):
+            return
 
-for row in rows:
-    print(row)
+        driver = 'ODBC Driver 17 for SQL Server'
+        if trusted_connection:
+            self.conn_str = (
+                f'DRIVER={{{driver}}};'
+                f'SERVER={server};'
+                f'DATABASE={database};'
+                f'Trusted_Connection=yes;'
+            )
+        else:
+            self.conn_str = (
+                f'DRIVER={{{driver}}};'
+                f'SERVER={server};'
+                f'DATABASE={database};'
+                f'UID={username};'
+                f'PWD={password};'
+            )
 
-conn.close()
+        self.conn = pyodbc.connect(self.conn_str)
+        self.initialized = True
+
+    def get_cursor(self):
+        if not self.conn:
+            self.conn = pyodbc.connect(self.conn_str)
+        return self.conn.cursor()
+
+    def close(self):
+        if self.conn:
+            self.conn.close()
+            self.conn = None
+
+
+connector = MSSQLConnector(server='localhost', database='brazilian_ecommerce')
+
+
+if __name__ == "__main__":
+    cursor = connector.get_cursor()
+
+    cursor.execute("SELECT TOP 5 name FROM sys.databases")
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.execute("SELECT TOP 5 name FROM sys.tables")
+    for row in cursor.fetchall():
+        print(row)
