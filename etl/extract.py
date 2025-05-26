d478601547effe40262893fae24ee0931acfadb3
Jakub Stępkowski <stepkos@wp.pl>
2025-05-26 02:26:26 +0200

    feat: extracting data

---
+++ etl/extract.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from pathlib import Path
+
+cache_path = Path(__file__).parent.parent / "cache"
+data_path = Path(__file__).parent.parent / "data"
+
+
+def extract_csv(file_path: str, cache: bool = True) -> pd.DataFrame:
+    cache_file = cache_path / f"{Path(file_path).stem}.pkl"
+    if cache and cache_file.exists():
+        print(f"Loading cached data from {cache_file}")
+        return pd.read_pickle(cache_file)
+
+    file_path = data_path / file_path
+    print(f"Extracting data from {file_path}")
+    df = pd.read_csv(file_path)
+    if cache:
+        df.to_pickle(cache_file)
+    return df
+
+
+if __name__ == "__main__":
+    df = extract_csv("customers.csv", cache=True)
+    print(df.head())
--- process.py
+++ process.py
@@ -4,10 +4,24 @@ from sql.drop_tables import DROP_TABLES_SQL
 
 cursor = connector.get_cursor()
 
+# Drop tables
 for drop_sql in DROP_TABLES_SQL:
     cursor.execute(drop_sql)
 
+# Create tables
 for create_sql in CREATE_TABLES_SQL:
     cursor.execute(create_sql)
 
+# 1.
+# PRODUCTS
+# CITIES
+# TIMESTAMPS (based on DIM_ORDERS, FACT_ORDER_ITEMS, DIM_REVIEWS)
+
+# 2.
+# ORDERS
+# REVIEWS
+# ORDER_ITEMS
+
 connector.close()
+
+
